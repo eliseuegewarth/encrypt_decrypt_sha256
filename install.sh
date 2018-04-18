12567b61ef982fd1bcb9747e2fd0991e6493340d
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-18 03:09:00 -0300

    Modificando a exportação do PATH
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-echo -e "\nexport PATH=$PATH:$(git rev-parse --show-toplevel)" >> ~/.bashrc
+echo -e "\nexport PATH=\$PATH:$(git rev-parse --show-toplevel)" >> ~/.bashrc
 rm -rf $(git rev-parse --show-toplevel)/.git/ $(git rev-parse --show-toplevel)/install.sh
